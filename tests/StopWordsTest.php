38f8621d227558e45397521afdd6879855847c11
Masroor Ehsan <masroore@gmail.com>
2022-06-12 13:44:40 +0200

    tests

---
+++ tests/StopWordsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaiju\Stopwords\Tests;
+
+use Kaiju\Stopwords\Stopwords;
+use PHPUnit\Framework\TestCase;
+
+class StopWordsTest extends TestCase
+{
+    /**
+     * @dataProvider provider
+     */
+    public function testFormat(string $language, string $original, string $clean): void
+    {
+        $stopWords = StopWords::make('', $language);
+        self::assertEquals($clean, $stopWords->clean($original));
+    }
+
+    public function provider(): array
+    {
+        return [
+            ['english', "Good muffins cost $3.88\nin New York.  Please buy me two of them.\n\nThanks!\n", 'Good muffins cost $3.88 New York Please buy two Thanks'],
+            ['spanish', 'ante a mi casa', 'casa'],
+        ];
+    }
+}
