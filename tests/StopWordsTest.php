d5f5883c6fc3d4e63c5132ea8189333a14baf189
Masroor Ehsan <masroore@gmail.com>
2022-06-12 13:58:44 +0200

    test for swedish language

--- tests/StopWordsTest.php
+++ tests/StopWordsTest.php
@@ -23,6 +23,7 @@ class StopWordsTest extends TestCase
         return [
             ['english', "Good muffins cost $3.88\nin New York.  Please buy me two of them.\n\nThanks!\n", 'Good muffins cost $3.88 New York Please buy two Thanks'],
             ['spanish', 'ante a mi casa', 'casa'],
+            ['swedish', 'Trädgårdsägare är beredda att pröva vad som helst för att bli av med de hatade mördarsniglarna åäö', 'Trädgårdsägare beredda pröva helst hatade mördarsniglarna åäö'],
         ];
     }
 }
